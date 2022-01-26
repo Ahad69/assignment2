0e78d151601b15795051708725d8538db936467e
Ahad69 <ahadxx99@gmail.com>
2022-01-26 01:55:24 -0600

    add

--- app.js
+++ app.js
@@ -11,15 +11,34 @@ form.addEventListener("submit", function (event) {
 	let emailValid = form.elements["email"];
 	// if valid, submit the form.
 	if (nameValid && emailValid) {
-		alert(nameValid.value);
+		alert("congrats");
         
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://sheetdb.io/api/v1/4ig4l29fr9p19", true);
+        xhr.open("POST", "https://boiling-waters-50126.herokuapp.com/addpurchase", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
-            name: nameValid,
-            email:emailValid
+            name: nameValid.value,
+            email: emailValid.value
         }));
+
+        var xhr1 = new XMLHttpRequest();
+        xhr1.open("POST", "https://chilling-phantom-97094.herokuapp.com/addbooking", true);
+        xhr1.setRequestHeader('Content-Type', 'application/json');
+        xhr1.send(JSON.stringify({
+            name: nameValid.value,
+            email: emailValid.value
+        }));
+
+        const scriptURL = "https://script.google.com/macros/s/AKfycbwffzFM1yise-YH63oHFH8MTJCpVAJjWht0X7PbDD5zOPwX8LDCRabJAEfJZ_yknY6lFg/exec";
+			const form = document.forms['google']
+		  
+			form.addEventListener('submit', e => {
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+				.then(response => alert('Success!', response))
+				.catch(error => console.error('Error!', error.message))
+        .reset()
+			})
 	}
 });
 
